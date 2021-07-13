6710b70e794f371301318ee7f3849d350b019e89
lobyden <mambadotaq@gmail.com>
2021-07-14 05:40:36 +0700

    Added a fontawesome to Project

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,35 @@
+import React from 'react';
+import { IconContext } from 'react-icons';
+import { FaGithub, FaEnvelope, FaBook} from "react-icons/fa";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <IconContext.Provider value = {{ color:"cornflowerblue", size: "1em"}}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
+        <div>
+          <FaBook />
+        </div>
+        
+        <div>
+          <FaEnvelope transform="rotate(180)"/>
+        </div>
+
+        <div>
+          <FaEnvelope />
+        </div>
+
+        <div>
+          <FaGithub />
+        </div>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +40,7 @@ function App() {
         </a>
       </header>
     </div>
+    </IconContext.Provider>
   );
 }
 
